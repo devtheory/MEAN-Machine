87d5e1c288d1463f61dbc6bf065376f647c9cec7
Jose Sanchez <jparkers0012@gmail.com>
2015-07-01 14:21:03 +0000

    set basic routes for admin dashboard, /users and /posts, adminRouter

--- server.js
+++ server.js
@@ -9,6 +9,29 @@ app.get('/', function(req, res){
     
 });
 
+// Create routes for the admin section
+
+// get an instance of the router
+var adminRouter = express.Router();
+
+// admin page. the dashboard http://localhost:1337/admin
+adminRouter.get('/', function(req, res){
+    res.send("This is the dashboard");
+});
+
+// users page. http://localhost:1337/admin/users
+adminRouter.get('/users', function(req, res){
+    res.send("Here are all the users");
+});
+
+// posts page. http://localhost:1337/admin/posts
+adminRouter.get('/posts', function(req, res){
+    res.send("Here are the posts");
+});
+
+// apply the routes to our application
+app.use('/admin', adminRouter); //admin creates the root for all routes defined for adminRouter
+
 // Fire da server!
 app.listen(process.env.PORT, process.env.IP);
 console.log("Le server iz listening");
