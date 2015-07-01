ab9ccbf043424c6b38e8eb00eb7ea4ade731996d
Jose Sanchez <jparkers0012@gmail.com>
2015-07-01 15:52:38 +0000

    Route middleware, params + validations

--- server.js
+++ server.js
@@ -14,6 +14,25 @@ app.get('/', function(req, res){
 // get an instance of the router
 var adminRouter = express.Router();
 
+// route middleware that will happen on every request
+adminRouter.use(function(req, res, next){ // defines middleware
+  // log each request to the console
+  console.log(req.method, req.url);
+  // and continue with routing
+  next();
+});
+
+// route middleware to validate :name
+adminRouter.param('name', function(req, res, next, name){
+  //validation stuff here
+  console.log("validating the name: " + name);
+  
+  //if validations pass, then save name as a param to the request
+  req.name = name;
+  
+  // and move on!
+  next();
+});
 // admin page. the dashboard http://localhost:1337/admin
 adminRouter.get('/', function(req, res){
     res.send("This is the dashboard");
@@ -22,6 +41,11 @@ adminRouter.get('/', function(req, res){
 // users page. http://localhost:1337/admin/users
 adminRouter.get('/users', function(req, res){
     res.send("Here are all the users");
+});
+
+// route with params in request
+adminRouter.get('/users/:name', function(req, res){
+  res.send("Hello, " + req.name + "!");
 });
 
 // posts page. http://localhost:1337/admin/posts
